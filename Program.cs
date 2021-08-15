6d0213a8aff1e4b266375a960ba418301ccc4a88
JoaoVictorYZ <joaovitor.jovas93@gmail.com>
2021-08-15 19:59:53 -0400

    the first one

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Joao_Victor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Alone!");
+        }
+    }
+}
