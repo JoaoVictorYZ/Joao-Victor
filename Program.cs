4fc0df8f3a0324fc89401a85256cfc173d5e9b8e
viniciusoliveira-boxti <vinicius.oliveira@boxti.com.br>
2021-08-15 22:15:39 -0300

    feat: few examples on conditionals

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Joao_Victor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Alone!");
+            Console.WriteLine("Qual sua idade?"); // Pergunta a idade do usuário
+            var userAge = int.Parse(Console.ReadLine()); // Salva a idade do usuário na variavel: userAge
+            // string texto = "";
+            // int inteiro = 0;
+            // double decimais = 0.0;
+            if (userAge > 18)
+            {
+                Console.WriteLine("Maior de 18");
+            }
+
+            // else if (userAge == 18)
+            // {
+            //     Console.WriteLine("Igual a 18");
+            // }
+
+            // else
+            // {
+            //     Console.WriteLine("Menor de 18");
+            // }
+
+            while (userAge > 18)
+            {
+                Console.WriteLine("loop infinito porquê userAge sempre será maior que 18");
+            }
         }
-    }
+    }  
 }
